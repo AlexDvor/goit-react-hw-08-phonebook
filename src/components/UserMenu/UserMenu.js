c43227738751e96bbbf06c9e72727da92e491d6a
Oleksii <alexmatvichuk@gmail.com>
2021-10-21 18:05:11 +0200

    add btn Bar

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,10 +1,21 @@
 import { UserMenuBox } from './UserMenu.styled';
+import Button from '@mui/material/Button';
 
 export default function UserMenu() {
   return (
     <UserMenuBox>
-      <p>User</p>
-      <button>LogOut</button>
+      <p>Alex</p>
+      <Button
+        variant="contained"
+        size="small"
+        sx={{
+          height: '30px',
+          marginLeft: '20px',
+          padding: '10px',
+        }}
+      >
+        LogOut
+      </Button>
     </UserMenuBox>
   );
 }
--- src/components/UserMenu/UserMenu.styled.jsx
+++ src/components/UserMenu/UserMenu.styled.jsx
@@ -1,5 +1,11 @@
 import styled from '@emotion/styled';
+// import Button from '@mui/material/Button';
 
 export const UserMenuBox = styled.div`
   display: flex;
+  align-items: center;
 `;
+// export const ButtonStyle = styled(Button)({
+//   paddingTop: '5px',
+//   paddingLeft: '10px',
+// });
