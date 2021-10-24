7357bd8e46200f0259988b17b1b421340a61fc84
Oleksii <alexmatvichuk@gmail.com>
2021-10-24 17:53:40 +0200

    add new form

--- src/pages/SignUpFormPage/SignUpFormPage.js
+++ src/pages/SignUpFormPage/SignUpFormPage.js
@@ -3,8 +3,7 @@ import { Formik } from 'formik';
 import { StyledForm, WrapperForm, StyledInput, StyledButton } from './SignUpFormPage.styled';
 import * as yup from 'yup';
 //
-import { TextField } from '@mui/material';
-// import Box from '@mui/material/Box';
+import { Button } from '@mui/material';
 
 export default function SignUpFormPage() {
   const validationSchema = yup.object().shape({
@@ -26,11 +25,7 @@ export default function SignUpFormPage() {
   return (
     <>
       <Formik
-        initialValues={{
-          email: '',
-          password: '',
-          confirmPassword: '',
-        }}
+        initialValues={initialValues}
         validatedOnBlur
         onSubmit={values => {
           console.log(values);
@@ -39,42 +34,48 @@ export default function SignUpFormPage() {
       >
         {({ values, errors, touched, handleChange, handleBlur, isValid, handleSubmit, dirty }) => (
           <WrapperForm>
-            <TextField
-              id="outlined-basic"
-              label="email"
-              variant="outlined"
-              type="email"
-              name="email"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.email}
-              autoComplete="true"
-            />
-            {touched.email && errors.email && <p>{errors.email}</p>}
+            <StyledForm onSubmit={handleSubmit}>
+              <StyledInput
+                id="outlined-basic"
+                label="email"
+                variant="outlined"
+                type="email"
+                name="email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+                autoComplete="true"
+              />
+              {touched.email && errors.email && <p>{errors.email}</p>}
 
-            <TextField
-              id="outlined-basic"
-              label="password"
-              variant="outlined"
-              type="password"
-              name="password"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.password}
-            />
-            {touched.password && errors.password && <p>{errors.password}</p>}
+              <StyledInput
+                id="outlined-basic"
+                label="password"
+                variant="outlined"
+                type="password"
+                name="password"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+              />
+              {touched.password && errors.password && <p>{errors.password}</p>}
 
-            <TextField
-              id="outlined-basic"
-              label="confirmPassword"
-              variant="outlined"
-              type="password"
-              name="confirmPassword"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.confirmPassword}
-            />
-            {touched.confirmPassword && errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+              <StyledInput
+                id="outlined-basic"
+                label="confirmPassword"
+                variant="outlined"
+                type="password"
+                name="confirmPassword"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.confirmPassword}
+              />
+              {touched.confirmPassword && errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+
+              <Button variant="contained" disabled={!isValid && !dirty} type="submit">
+                Registration
+              </Button>
+            </StyledForm>
 
             {/* <StyledForm onSubmit={handleSubmit}>
               <label htmlFor={'email'}>Email</label>
--- src/pages/SignUpFormPage/SignUpFormPage.styled.jsx
+++ src/pages/SignUpFormPage/SignUpFormPage.styled.jsx
@@ -1,19 +1,29 @@
 import styled from '@emotion/styled';
+import { TextField } from '@mui/material';
 
-// export const StyledForm = styled.form`
-//   display: flex;
-//   flex-direction: column;
-//   width: 250px;
-// `;
+export const StyledForm = styled.form`
+  /* display: flex;
+  flex-direction: column;
+  width: 250px; */
+`;
+
+export const StyledInput = styled(TextField)`
+  &:not(:first-child) {
+    margin-top: 20px;
+  }
+`;
 
 export const WrapperForm = styled.div`
   display: flex;
-
   margin-top: 50px;
   width: 250px;
   flex-direction: column;
   margin-left: auto;
   margin-right: auto;
+
+  padding: 40px;
+  border: 1px solid #1976d2;
+  border-radius: 2%;
 `;
 // export const StyledInput = styled.input`
 //   &:not(:first-child) {
