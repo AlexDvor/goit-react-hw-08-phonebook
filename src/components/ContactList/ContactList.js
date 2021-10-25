89063a55deead7765acded11a6b850c2861a0fe2
Oleksii <alexmatvichuk@gmail.com>
2021-10-25 19:21:56 +0200

    bug-fix contact form

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,12 +1,9 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { ButtonStyled } from './ContactList.styled';
+// import { ButtonStyled } from './ContactList.styled';
 import * as contactsOperations from '../../redux/Contact/contacts-operations';
-import {
-  visibleContactsSelector,
-  getContactsSelector,
-} from '../../redux/Contact/contacts-selectors';
+import { visibleContactsSelector } from '../../redux/Contact/contacts-selectors';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -14,8 +11,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import Filter from '../Filter/Filter';
+// import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 export default function ContactList() {
   const dataUsers = useSelector(visibleContactsSelector);
@@ -26,32 +22,29 @@ export default function ContactList() {
   }, [dispatch]);
 
   return (
-    <>
-      <Filter></Filter>
-      <TableContainer component={Paper} sx={{ width: '50%' }}>
-        <Table sx={{ minWidth: 250 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell align="right">Phone</TableCell>
+    <TableContainer component={Paper} sx={{ width: '50%' }}>
+      <Table sx={{ minWidth: 250 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Phone</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {dataUsers.map(row => (
+            <TableRow key={row.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.phone}</TableCell>
+              {/* <ButtonStyled onClick={() => dispatch(contactsOperations.deleteContacts(row.id))}>
+                <DeleteForeverIcon sx={{ color: '#3498db' }}></DeleteForeverIcon>
+              </ButtonStyled> */}
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {dataUsers.map(row => (
-              <TableRow key={row.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.phone}</TableCell>
-                <ButtonStyled onClick={() => dispatch(contactsOperations.deleteContacts(row.id))}>
-                  <DeleteForeverIcon sx={{ color: '#3498db' }}></DeleteForeverIcon>
-                </ButtonStyled>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
