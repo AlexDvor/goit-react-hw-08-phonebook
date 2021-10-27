9ea59a92fa7d8a001c90075096744316dfd26bae
Oleksii <alexmatvichuk@gmail.com>
2021-10-27 17:39:55 +0200

    bug-fix registr form

--- src/pages/SignUpFormPage/SignUpFormPage.js
+++ src/pages/SignUpFormPage/SignUpFormPage.js
@@ -18,9 +18,17 @@ export default function SignUpFormPage() {
   };
 
   const validationSchema = yup.object().shape({
-    name: yup.string().typeError('Should be a string').required('This field is required'),
+    name: yup
+      .string()
+      .typeError('Should be a string')
+      .matches(/^[A-Za-z ]*$/, 'Please enter valid name')
+      .required('This field is required'),
     email: yup.string().email('Please enter a valid email').required('This field is required'),
-    password: yup.string().typeError('Should be a string').required('This field is required'),
+    password: yup
+      .string()
+      .typeError('Should be a string')
+      .min(7, 'Password is too short - should be 7 chars minimum.')
+      .required('This field is required'),
     confirmPassword: yup
       .string()
       .oneOf([yup.ref('password')], 'Passwords must match')
