60a6307a2380dde67e1abaa2d0c467a21ba9761c
Oleksii <alexmatvichuk@gmail.com>
2021-10-27 19:24:28 +0200

    add styles contact

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -3,16 +3,19 @@ import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import * as contactsOperations from '../../redux/Contact/contacts-operations';
 import { visibleContactsSelector } from '../../redux/Contact/contacts-selectors';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import { getIsLoggedIn } from '../../redux/Auth/Auth-selector';
-// import { ButtonStyled } from './ContactList.styled';
-// import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import {
+  ButtonStyled,
+  ContentWrapper,
+  HeadingTitleBox,
+  ListItems,
+  UserIconBox,
+  Item,
+  Name,
+  Phone,
+} from './ContactList.styled';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import PersonIcon from '@mui/icons-material/Person';
 
 export default function ContactList() {
   const dataUsers = useSelector(visibleContactsSelector);
@@ -27,29 +30,32 @@ export default function ContactList() {
   }, [dispatch, isLoggedIn]);
 
   return (
-    <TableContainer component={Paper} sx={{ width: '50%' }}>
-      <Table sx={{ minWidth: 250 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Phone</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {dataUsers.map(row => (
-            <TableRow key={row.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.number}</TableCell>
-              {/* <ButtonStyled onClick={() => dispatch(contactsOperations.deleteContacts(row.id))}>
-                <DeleteForeverIcon sx={{ color: '#3498db' }}></DeleteForeverIcon>
-              </ButtonStyled> */}
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <ContentWrapper>
+      <HeadingTitleBox>
+        <div>Name</div>
+        <div>Phone </div>
+      </HeadingTitleBox>
+      <ListItems>
+        {dataUsers.length > 0 ? (
+          dataUsers.map(item => (
+            <Item>
+              <UserIconBox>
+                <PersonIcon></PersonIcon>
+              </UserIconBox>
+              <Name>{item.name}</Name>
+              <Phone>{item.number}</Phone>
+              <div>
+                <ButtonStyled onClick={() => dispatch(contactsOperations.deleteContacts(item.id))}>
+                  <DeleteForeverIcon sx={{ color: '#3498db' }}></DeleteForeverIcon>
+                </ButtonStyled>
+              </div>
+            </Item>
+          ))
+        ) : (
+          <h1>Your list is empty</h1>
+        )}
+      </ListItems>
+    </ContentWrapper>
   );
 }
 
--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -1,9 +1,47 @@
 import styled from '@emotion/styled';
 
+export const ContentWrapper = styled.div`
+  width: 670px;
+
+  /* outline: 1px solid black; */
+`;
+export const ListItems = styled.ul`
+  /* outline: 1px solid red; */
+  /* width: 250px; */
+`;
+export const Item = styled.li`
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+  outline: 1px solid black;
+  padding: 13px;
+  border-radius: 10px;
+  box-shadow: 0 2px 10px 0 rgb(37 50 56 / 10%);
+  justify-content: space-between;
+  transition: transform 100ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover,
+  :focus {
+    transform: scale(1.01);
+  }
+`;
+export const HeadingTitleBox = styled.div`
+  display: flex;
+  border: 1px solid black;
+  justify-content: space-around;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: rgb(73, 151, 230);
+  color: white;
+`;
 export const ButtonStyled = styled.button`
-  margin-left: 20px;
+  display: flex;
   background-color: transparent;
   border: none;
   padding: 5px;
   cursor: pointer;
 `;
+
+export const UserIconBox = styled.div``;
+export const Name = styled.div``;
+export const Phone = styled.div``;
